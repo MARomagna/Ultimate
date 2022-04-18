ccbe07876c577e90e11a446918669064fcfc077d
UltimateZ <maromagna@gmail.com>
2022-04-18 20:49:06 -0300

    Ultimate

---
+++ js/formulario.js
@@ -0,0 +1,22 @@
+const formulario = document.querySelector("form");
+function formularioEnviado(resposta) {
+  if (resposta.ok) {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317a00;'> Mensagem enviada</span> com sucesso</p>";
+  } else {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'> Erro no envio</p>";
+  }
+}
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector("form button");
+  botao.disable = true;
+  botao.innerText = "Enviando...";
+  const data = new FormData(formulario);
+  fetch("./enviar.php", {
+    method: "POST",
+    body: data,
+  }).then(formularioEnviado);
+}
+formulario.addEventListener("submit", enviarFormulario);
